37ce7b4df0e88d27b198fe2bae5e18047228ca3f
vibha-mishra-unthinkable <vibha.mishra@unthinkable.co>
2025-06-20 16:28:11 +0530

    catch-all route

---
+++ app/(routes)/users/[...id]/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+//catch-all route for dynamic user IDs
+//This will match any route like /users/1, /users/2, /users/3, etc.
+//and will pass the IDs as an array to the component
+//e.g. /users/1/2/3 will pass { id: ['1', '2', '3'] } to the component
+
+//This is a catch-all route, so it will match any number of segments after /users/
+//If you want to match only one segment, use [id].tsx instead of [...id].tsx
+export default function UserIdsPage({ params }: { params: { id: string[] } }) {
+    console.log('User IDs:', params);
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Dynamic Users Path</h1>
+      <p className="mt-2 text-gray-700">
+        IDs from the URL: <strong>{params.id?.join(', ')}</strong>
+      </p>
+    </div>
+  );
+}
